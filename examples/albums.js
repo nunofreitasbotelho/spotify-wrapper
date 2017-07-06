3438c8bed8d5536e8d4db9036db863371c97f287
nunofreitasbotelho <nunofreitasbotelho@gmail.com>
2017-07-06 04:25:20 +0100

    Refactoring to use Token

--- examples/albums.js
+++ examples/albums.js
@@ -1,7 +1,11 @@
 global.fetch = require('node-fetch');
 
-import { searchAlbums } from '../src/search';
+import SpotifyWrapper from '../src/index';
 
-const albums = searchAlbums('Incubus');
+const spotify = new SpotifyWrapper({
+  token: 'BQA-Uh_G70MwKgzh0Z_MRA85Geb_GQy5QVyHFzGVb91TlpLu8A763oEa25YHS55XPNXZrPxAF68dxUBED9GXGBv3Jc4xk8VFVqVQ6uQ45Apw6GkxRK_BiyDOPazKFPQfSrz8dibNKMwS0Bh_NGFgag',
+});
+
+const albums = spotify.search.albums('Incubus');
 
 albums.then(data => data.albums.items.map(item => console.log(item.name)));
